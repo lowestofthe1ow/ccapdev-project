5589a52de9e3ac63637d03dbb406ca66215e1b9a
lowestofthe1ow <1owestoftheow@gmail.com>
2025-01-29 00:12:42 +0800

    Move back files not yet converted to .handlebars; prepare views folder for serving with express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import Handlebars from "handlebars";
+import { engine } from "express-handlebars";
 
 const app = express();
 const port = 8000;
 
-const client = new MongoClient(process.env.MONGODB_URI);
+// const client = new MongoClient(process.env.MONGODB_URI);
 
-/* Sample function for accessing MongoDB database */
-async function mongodb_test() {
-    try {
-        const database = client.db("shinosawa");
-        const users = database.collection("users");
+/* Use the handlebars engine */
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
-        /* Sample query */
-        const user = await users.findOne({ username: "lowestofthelow" });
+/* Set src/views as the views directory */
+app.set("views", "src/views");
 
-        console.log(user);
-    } finally {
-        await client.close();
-    }
-}
-
-app.use(express.static("public"));
+/* Serve static content from the public directory */
+app.use("/", express.static("public"));
 
 app.listen(port, () => {
-    mongodb_test();
     console.log(`Listening on port ${port}.`);
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
 });
