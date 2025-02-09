712db461c08089e576280e7f4706e11b961e9a03
lowestofthe1ow <1owestoftheow@gmail.com>
2025-02-09 14:06:43 +0800

    Set up comment/reply posting

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -2,10 +2,10 @@ window.addEventListener("load", () => {
     /* Apply to all buttons that "toggle" the visibility of some component */
     this.document.querySelectorAll("[data-toggle]").forEach((x) => {
         x.addEventListener("click", () => {
+            /* TODO: Make this settable */
+            x.classList.toggle("button--current");
             /* Apply the "hidden" class */
-            this.document
-                .querySelector(x.dataset.toggle)
-                .classList.toggle("hidden");
+            this.document.querySelector(x.dataset.toggle).classList.toggle("hidden");
         });
     });
 });
