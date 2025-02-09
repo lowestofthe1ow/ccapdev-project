9bf275852bf5205d87f5ac2f362a5d5a5868a39f
lowestofthe1ow <1owestoftheow@gmail.com>
2025-02-09 09:58:55 +0800

    Add formatting for images in posts

--- src/helpers/markdown.js
+++ src/helpers/markdown.js
@@ -1,6 +1,10 @@
 import markdownit from "markdown-it";
+import markdownitclass from "markdown-it-class";
 
-const md = markdownit();
+const md = markdownit().use(markdownitclass, {
+    img: ["article__image"],
+    a: ["article__link"],
+});
 
 export default (input) => {
     return md.render(input);
