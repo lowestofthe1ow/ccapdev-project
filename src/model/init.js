f08479626b01f9b1e10d6b6de37f06bbbf524f14
lowestofthe1ow <1owestoftheow@gmail.com>
2025-02-10 16:25:20 +0800

    Solidify database schema

---
+++ src/model/init.js
@@ -0,0 +1,22 @@
+import db_conn from "./db.js";
+
+/* Schema validators */
+import comments from "./schemas/comments.js";
+
+db_conn.connect().then(async () => {
+    let db = db_conn.db(process.env.MONGODB_DBNAME);
+
+    /* Create the `comments` collection */
+    await db.collection("comments").drop();
+    await db.createCollection("comments", {
+        validator: comments,
+    });
+
+    /* Create the `threads` collection */
+    await db.collection("threads").drop();
+    await db.createCollection("threads", {
+        validator: comments,
+    });
+
+    db_conn.close();
+});
