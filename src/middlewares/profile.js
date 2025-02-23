6d77fbd6a3765e133d35e048d9e543ebf153f4af
lowestofthe1ow <1owestoftheow@gmail.com>
2025-02-24 00:08:12 +0800

    Clean up profile

--- src/middlewares/profile.js
+++ src/middlewares/profile.js
@@ -2,28 +2,13 @@ import { ObjectId } from "mongodb";
 
 export async function get_user(req, res, next) {
     try {
-        /* Fetch from database */
-        /*
-        let _user = req.app.get("db").collection("user");
-        let user = await _user
-            .aggregate([{ $sort: { id: -1 } }])
-            .toArray();
-        */
-
-        let _user = req.app.get("db").collection("user").findOne({});
+        /* Just get one result for now */
+        let _user = req.app.get("db").collection("users").findOne({});
         let user = await _user;
 
-        // {banner: "/img/banner_gkms.jpg", pfp: "/img/pfp.png", username: "lowestofthelow", bio: "weeb"};
-
         /* Apply to request object */
-
         req.app.set("user", user);
-        /*
-        req.app.set("banner", "/img/banner_gkms.jpg");
-        req.app.set("pfp", "/img/pfp.png");
-        req.app.set("username", _user.username);
-        req.app.set("bio", "weeb")
-        */
+
         next();
     } catch (error) {
         console.error(error);
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,20 +1,20 @@
 import express from "express";
-import { ObjectId } from "mongodb";
+
+/* Helpers */
+import format_date from "../helpers/format_date.js";
 
 /* Middleware */
 import { get_user } from "../middlewares/profile.js";
+import { get_threads } from "../middlewares/get_threads.js";
 
 const router = express.Router();
 
-router.get("/", get_user, (req, res) => {
+router.get("/", get_threads, get_user, (req, res) => {
     res.render("profile", {
-        user: req.app.get("user")
-        /*
-        banner: req.app.get("banner"),
-        pfp: req.app.get("pfp"),
-        username: req.app.get("username"),
-        bio: req.app.get("bio")
-        */
+        user: req.app.get("user"),
+        layout: "forum",
+        threads: req.app.get("threads"),
+        helpers: { format_date },
     });
 });
 
