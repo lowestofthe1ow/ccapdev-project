5264c96f803760dcec9f60fc5ce458d8adb3f001
lowestofthe1ow <1owestoftheow@gmail.com>
2025-02-23 22:12:41 +0800

    Clean up Markdown rendering / implement text editor preview

--- src/helpers/markdown.js
+++ src/helpers/markdown.js
@@ -1,17 +1,9 @@
 import markdownit from "markdown-it";
-import markdownitclass from "markdown-it-class";
 
-const md = markdownit().use(markdownitclass, {
-    img: ["article__image"],
-    a: ["article__link"],
-});
+const md = markdownit();
 
 /**
- * Renders the text input as Markdown with {@link markdownit.render}. Note that the following elements are assigned
- * special classes:
- *
- * - `<img>`: `article__image`
- * - `<a>`: `article__link`
+ * Renders the text input as Markdown with {@link markdownit.render}.
  *
  * @param input - The text input in Markdown
  */
