275607d3fbcb979aa115a7fb4d12ae5bfd781877
lowestofthe1ow <1owestoftheow@gmail.com>
2025-02-25 13:22:06 +0800

    Fix merge issues

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,15 +1,16 @@
 import express from "express";
 
 /* Helpers */
-import format_date from "../helpers/format_date.js";
+import check_depth from "../helpers/check_depth.js";
 import check_id from "../helpers/check_id.js";
 import concat from "../helpers/concat.js";
+import format_date from "../helpers/format_date.js";
 import markdown from "../helpers/markdown.js";
 
 /* Middleware */
 import get_active_user from "../middlewares/get_active_user.js";
-import { get_threads } from "../middlewares/get_threads.js";
 import { get_thread_comments } from "../middlewares/get_comments.js";
+import { get_threads } from "../middlewares/get_threads.js";
 
 const router = express.Router();
 
@@ -25,37 +26,47 @@ router.get(
     (req, res) => {
         res.render("profile", {
             layout: "forum",
-            threads: req.app.get("threads"),
             helpers: { format_date },
         });
     }
 );
 
-router.get("/comments", get_thread_comments, get_user, (req, res) => {
-    res.render("pfcomments", {
-        user: req.app.get("user"),
-        layout: "forum",
-        threads: req.app.get("threads"),
-        comments: req.app.get("comments"),
-        helpers: {
-            check_depth(depth) {
-                return depth >= 3;
+router.get(
+    "/comments",
+    /* TODO: Replace this with session middleware */
+    (req, res, next) => {
+        req.username = "lowestofthelow";
+        next();
+    },
+    get_active_user /* Gets the current active user */,
+    (req, res) => {
+        res.render("pfcomments", {
+            layout: "forum",
+            helpers: {
+                check_depth,
+                check_id,
+                concat,
+                format_date,
+                markdown,
             },
-            check_id,
-            concat,
-            format_date,
-            markdown,
-        }
-    });
-});
+        });
+    }
+);
 
-router.get("/upvoted", get_threads, get_user, (req, res) => {
-    res.render("pfupvoted", {
-        user: req.app.get("user"),
-        layout: "forum",
-        threads: req.app.get("threads"),
-        helpers: { format_date },
-    });
-});
+router.get(
+    "/upvoted",
+    get_threads /* TODO: Replace this with session middleware */,
+    (req, res, next) => {
+        req.username = "lowestofthelow";
+        next();
+    },
+    get_active_user /* Gets the current active user */,
+    (req, res) => {
+        res.render("pfupvoted", {
+            layout: "forum",
+            helpers: { format_date },
+        });
+    }
+);
 
 export default router;
