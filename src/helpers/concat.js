93d54bb375ef9b202cd7e1aaa1aa838ffad6b10d
lowestofthe1ow <1owestoftheow@gmail.com>
2025-03-01 10:52:33 +0800

    Clean up concat helper

--- src/helpers/concat.js
+++ src/helpers/concat.js
@@ -1,9 +1,7 @@
 /**
  * Helper function that concatenates two strings, for use in Handlebars
- *
- * @param string1 - The first string
- * @param string2 - The second string
  */
-export default (string1, string2) => {
-    return string1 + string2;
+export default (...args) => {
+    args.pop(); // The last argument is some weird Handlebars thing
+    return args.join("");
 };
