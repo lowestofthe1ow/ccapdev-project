b5e223ac288ec695467a84c5c2639fe50a9e9c3a
lowestofthe1ow <1owestoftheow@gmail.com>
2025-03-04 23:22:01 +0800

    Updated scrolling ads (now clickable and pulls from games)

--- src/middlewares/get_ads.js
+++ src/middlewares/get_ads.js
@@ -1,12 +1,9 @@
 export default async (req, res, next) => {
+    const adsCollection = req.app.get("db").collection("featured_games");
 
-    const adsCollection = req.app.get("db").collection("ads");
-    
-    const ads = await adsCollection
-        .find({}, { projection: { _id: 0, link: 1 } })
-        .toArray();
+    const ads = await adsCollection.aggregate([{ $sample: { size: 5 } }]).toArray();
 
-    res.locals.images = ads; 
+    res.locals.images = ads;
 
     next();
-}
+};
